20f8904afd96f1dfe2b575d478f7f83015ad6777
Arckanna <valerie.lecoeur75@gmail.com>
2024-01-12 21:40:24 +0100

    Two first class
    Creation of class document and class quote

--- src/business/Document.java
+++ src/business/Document.java
@@ -1,5 +1,82 @@
 package business;
 
-public class Document {
+import java.time.LocalDateTime;
 
+public class Document {
+    private Client client;
+    private LocalDateTime dateCreation;
+
+    // Constructor
+    protected Document() {
+        this.dateCreation = LocalDateTime.now();
+    }
+
+    // Constructor with parameters of type Client
+    protected Document(Client client) {
+        this();
+        this.client = client;
+
+    }
+
+    // Constructor with parameters of type Client and LocalDateTime
+    protected Document(Client client, LocalDateTime dateCreation) {
+        this(client);
+        this.dateCreation = dateCreation;
+    }
+
+    // Getters and Setters
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public LocalDateTime getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(LocalDateTime dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Document [client=" + client + ", dateCreation=" + dateCreation + "]";
+    }
+
+    // equals method
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Document))
+            return false;
+        Document other = (Document) obj;
+        if (client == null) {
+            if (other.client != null)
+                return false;
+        } else if (!client.equals(other.client))
+            return false;
+        if (dateCreation == null) {
+            if (other.dateCreation != null)
+                return false;
+        } else if (!dateCreation.equals(other.dateCreation))
+            return false;
+        return true;
+    }
+
+    // hashCode method
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((client == null) ? 0 : client.hashCode());
+        result = prime * result + ((dateCreation == null) ? 0 : dateCreation.hashCode());
+        return result;
+    }
 }
--- src/business/Quote.java
+++ src/business/Quote.java
@@ -1,5 +1,77 @@
 package business;
 
-public class Quote {
+import java.time.LocalDate;
+
+public class Quote extends Document {
+
+    private LocalDate dateExpiration;
+    private Long reference;
+    private static Long nextReference = 0L;
+
+    public Quote(LocalDate dateExpiration) {
+        super();
+        this.dateExpiration = dateExpiration;
+        this.reference = ++nextReference;
+
+    }
+
+    public Quote(Client client, LocalDate dateExpiration) {
+        this(dateExpiration);
+        super.setClient(client);
+
+    }
+
+    // Getters and Setters
+    public LocalDate getDateExpiration() {
+        return dateExpiration;
+    }
+
+    public void setDateExpiration(LocalDate dateExpiration) {
+        this.dateExpiration = dateExpiration;
+    }
+
+    public Long getReference() {
+        return reference;
+    }
+
+    public void setReference(Long reference) {
+        this.reference = reference;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Quote [dateExpiration=" + dateExpiration + ", reference=" + reference + ", toString()="
+                + super.toString() + "]";
+    }
+
+    // equals method
+    @Override
+    public boolean equals(Object obj) {
+        if (!super.equals(obj))
+            return false;
+        Quote other = (Quote) obj;
+        if (dateExpiration == null) {
+            if (other.dateExpiration != null)
+                return false;
+        } else if (!dateExpiration.equals(other.dateExpiration))
+            return false;
+        if (reference == null) {
+            if (other.reference != null)
+                return false;
+        } else if (!reference.equals(other.reference))
+            return false;
+        return true;
+    }
+
+    // hashCode method
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((dateExpiration == null) ? 0 : dateExpiration.hashCode());
+        result = prime * result + ((reference == null) ? 0 : reference.hashCode());
+        return result;
+    }
 
 }
